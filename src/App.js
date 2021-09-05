dad40f8cf9754a7586d06672c9392faec1436863
krishsam94 <krishsam94@gmail.com>
2021-09-05 12:22:13 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
       <h1 data-testid="helloCls">Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+        data-testid="h2Clk"
+      >
+        Start editing to see some magic happen!
+      </h2>
+
+      <div data-testid="result">{counter}</div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 import React from "react";
+import userEvent from "@testing-library/user-event";
 
 test("testId", () => {
   render(<div data-testid="custom-element" />);
@@ -9,14 +10,24 @@ test("testId", () => {
   expect(element).toBeInTheDocument();
 });
 
-test("renders learn react link", () => {
+/* test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText("Hello");
   expect(linkElement).toBeInTheDocument();
-});
+}); */
 
 test("renders learn react link 2", () => {
   render(<App />);
   const linkElement = screen.getByTestId("helloCls");
   expect(linkElement).toHaveTextContent(/hello/i);
+});
+
+test("test click", () => {
+  render(<App />);
+  const clickEle = screen.getByTestId("h2Clk");
+  userEvent.click(clickEle);
+
+  const resDom = screen.getByTestId("result");
+
+  expect(resDom).toHaveTextContent(1);
 });
